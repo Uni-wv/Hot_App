5e9d08ebc9111b3f402d64709d4f96734c31bc0d
Uni-wv <65159528+Uni-wv@users.noreply.github.com>
2020-07-25 15:44:06 +0530

    Main Program File

---
+++ main.dart
@@ -0,0 +1,12 @@
+import 'package:Hot_App/ui/home.dart';
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(hotcon());
+}
+
+class hotcon extends StatelessWidget {
+  build(BuildContext con) {
+    return MyApp();
+  }
+}
